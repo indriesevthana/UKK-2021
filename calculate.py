4374778fa7214197d7a9f35967f7f5bd2ab9f8e5
indriesevthana <indzzhrr03@gmail.com>
2021-11-09 09:34:17 +0700

    kalkulator

---
+++ calculate.py
@@ -0,0 +1,254 @@
+from tkinter import *
+import tkinter.messagebox
+import math
+
+
+root = Tk()
+root.geometry("650x400+300+300")
+root.title("Kalkulatorku")
+
+switch = None
+
+# Button on press
+
+
+def btn1_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '1')
+
+
+def btn2_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '2')
+
+
+def btn3_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '3')
+
+
+def btn4_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '4')
+
+
+def btn5_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '5')
+
+
+def btn6_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '6')
+
+
+def btn7_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '7')
+
+
+def btn8_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '8')
+
+
+def btn9_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '9')
+
+
+def btn0_clicked():
+    if disp.get() == '0':
+        disp.delete(0, END)
+    pos = len(disp.get())
+    disp.insert(pos, '0')
+
+
+def key_event(*args):
+    if disp.get() == '0':
+        disp.delete(0, END)
+
+
+def btnp_clicked():
+    pos = len(disp.get())
+    disp.insert(pos, '+')
+
+
+def btnm_clicked():
+    pos = len(disp.get())
+    disp.insert(pos, '-')
+
+
+def btnml_clicked():
+    pos = len(disp.get())
+    disp.insert(pos, '*')
+
+
+def btnd_clicked():
+    pos = len(disp.get())
+    disp.insert(pos, '/')
+
+
+def btnc_clicked(*args):
+    disp.delete(0, END)
+    disp.insert(0, '0')
+
+def dot_clicked():
+    pos = len(disp.get())
+    disp.insert(pos, '.')
+
+
+def del_clicked():
+    pos = len(disp.get())
+    display = str(disp.get())
+    if display == '':
+        disp.insert(0, '0')
+    elif display == ' ':
+        disp.insert(0, '0')
+    elif display == '0':
+        pass
+    else:
+        disp.delete(0, END)
+        disp.insert(0, display[0:pos-1])
+
+
+def mod_clicked():
+    pos = len(disp.get())
+    disp.insert(pos, '%')
+
+
+def btneq_clicked(*args):
+    try:
+        ans = disp.get()
+        ans = eval(ans)
+        disp.delete(0, END)
+        disp.insert(0, ans)
+
+    except:
+        tkinter.messagebox.showerror("Value Error", "Check your values and operators")
+
+
+# Label
+
+
+# button
+
+disp = Entry(root, font="Verdana 20", fg="white", bg="#BDB76B", bd=0, justify=RIGHT, insertbackground="#BDB76B", cursor="arrow")
+disp.bind("<Return>", btneq_clicked)
+disp.bind("<Escape>", btnc_clicked)
+disp.bind("<Key-1>", key_event)
+disp.bind("<Key-2>", key_event)
+disp.bind("<Key-3>", key_event)
+disp.bind("<Key-4>", key_event)
+disp.bind("<Key-5>", key_event)
+disp.bind("<Key-6>", key_event)
+disp.bind("<Key-7>", key_event)
+disp.bind("<Key-8>", key_event)
+disp.bind("<Key-9>", key_event)
+disp.bind("<Key-0>", key_event)
+disp.bind("<Key-.>", key_event)
+disp.insert(0, '0')
+disp.focus_set()
+disp.pack(expand=TRUE, fill=BOTH)
+
+# Row 1 Buttons
+
+btnrow1 = Frame(root, bg="#000000")
+btnrow1.pack(expand=TRUE, fill=BOTH)
+
+btnc = Button(btnrow1, text="C", font="Segoe 23", relief=GROOVE, bd=0, command=btnc_clicked, fg="black", bg="#FFF8DC")
+btnc.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn1 = Button(btnrow1, text="1", font="Segoe 23", relief=GROOVE, bd=0, command=btn1_clicked, fg="black", bg="#FFF8DC")
+btn1.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn2 = Button(btnrow1, text="2", font="Segoe 23", relief=GROOVE, bd=0,  command=btn2_clicked, fg="black", bg="#FFF8DC")
+btn2.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn3 = Button(btnrow1, text="3", font="Segoe 23", relief=GROOVE, bd=0, command=btn3_clicked, fg="black", bg="#FFF8DC")
+btn3.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btnp = Button(btnrow1, text="+", font="Segoe 23", relief=GROOVE, bd=0, command=btnp_clicked, fg="black", bg="#FFF8DC")
+btnp.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+
+# Row 2 Buttons
+
+btnrow2 = Frame(root)
+btnrow2.pack(expand=TRUE, fill=BOTH)
+
+mod_btn = Button(btnrow2, text="%", font="Segoe 21", relief=GROOVE, bd=0, command=mod_clicked, fg="black", bg="#FFF8DC")
+mod_btn.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+
+btn4 = Button(btnrow2, text="4", font="Segoe 23", relief=GROOVE, bd=0, command=btn4_clicked, fg="black", bg="#FFF8DC")
+btn4.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn5 = Button(btnrow2, text="5", font="Segoe 23", relief=GROOVE, bd=0, command=btn5_clicked, fg="black", bg="#FFF8DC")
+btn5.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn6 = Button(btnrow2, text="6", font="Segoe 23", relief=GROOVE, bd=0, command=btn6_clicked, fg="black", bg="#FFF8DC")
+btn6.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btnm = Button(btnrow2, text="-", font="Segoe 23", relief=GROOVE, bd=0, command=btnm_clicked, fg="black", bg="#FFF8DC")
+btnm.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+# Row 3 Buttons
+
+btnrow3 = Frame(root)
+btnrow3.pack(expand=TRUE, fill=BOTH)
+
+btnd = Button(btnrow3, text="/", font="Segoe 23", relief=GROOVE, bd=0, command=btnd_clicked, fg="black", bg="#FFF8DC")
+btnd.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn7 = Button(btnrow3, text="7", font="Segoe 23", relief=GROOVE, bd=0, command=btn7_clicked, fg="black", bg="#FFF8DC")
+btn7.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn8 = Button(btnrow3, text="8", font="Segoe 23", relief=GROOVE, bd=0, command=btn8_clicked, fg="black", bg="#FFF8DC")
+btn8.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn9 = Button(btnrow3, text="9", font="Segoe 23", relief=GROOVE, bd=0, command=btn9_clicked, fg="black", bg="#FFF8DC")
+btn9.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btnml = Button(btnrow3, text="*", font="Segoe 23", relief=GROOVE, bd=0, command=btnml_clicked, fg="black", bg="#FFF8DC")
+btnml.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+# Row 4 Buttons
+
+btnrow4 = Frame(root)
+btnrow4.pack(expand=TRUE, fill=BOTH)
+
+dot_btn = Button(btnrow4, text=" • ", font="Segoe 21", relief=GROOVE, bd=0, command=dot_clicked, fg="black", bg="#FFF8DC")
+dot_btn.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+
+del_btn = Button(btnrow4, text="⌫", font="Segoe 20", relief=GROOVE, bd=0, command=del_clicked, fg="black", bg="#FFF8DC")
+del_btn.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btn0 = Button(btnrow4, text="0", font="Segoe 23", relief=GROOVE, bd=0, command=btn0_clicked, fg="black", bg="#FFF8DC")
+btn0.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+btneq = Button(btnrow4, text="=", font="Segoe 23", relief=GROOVE, bd=0, command=btneq_clicked, fg="black", bg="#FFF8DC")
+btneq.pack(side=LEFT, expand=TRUE, fill=BOTH)
+
+
+
+root.mainloop()
